d3d1e99e2099198c6e386eac3b0491a43d74af13
Ofir Cohen <ofir5300@gmail.com>
2020-04-11 21:27:21 +0300

    setPartyViewComponents is set with firestore

--- App.js
+++ App.js
@@ -10,35 +10,10 @@ export default function App() {
         <TextInput />
         <Button title="ADD" />
         <PartyView />
-
-        <Button
-        onPress={onPressPlayPause}
-        title="Play Pause"
-        color="#841584"
-        accessibilityLabel="Play Pause" 
-        />
       </View>
       <View></View>
     </View>
   );
-}
-const onPressPlayPause = async () => {
-  const db = firebase.firestore()
-  try {
-    const party = await db.collection('parties').doc('obQG8hZEirCDw2uMkP17').get()
-    const currentCondition = party.data().condition
-
-    const newCondition = invertCondition(currentCondition)
-    // this.condition = newCondition
-  await db.collection('parties').doc('obQG8hZEirCDw2uMkP17').update({condition: newCondition})
-  }
-  catch (e) {
-    console.log(e.message)
-  }
-}
-
-const invertCondition = (cond) => {
-return cond === 'play' ? 'pause' : 'play'
 }
 
 // const styles = StyleSheet.create({
