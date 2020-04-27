496b37fddca23406805768eafa6d08b5ebbd1eeb
amitb <amitbu@mta.ac.il>
2020-04-27 19:27:57 +0300

    webview manipulation successful, without disabling taps on player

--- app/views/subComponents/YoutubePlayer.js
+++ app/views/subComponents/YoutubePlayer.js
@@ -1,20 +1,67 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class YoutubePlayer extends React.Component {
     constructor(props) {
         super(props);
+        console.log(props)
+    }
+
+    onPlayerReady = event => {
+        if(this.props.condition === 'play') {
+            event.target.playVideo();
+        }
     }
 
     render() {
-        let youtubeUrl = `https://www.youtube.com/embed/${this.props.videoId}`;
-        console.log('youtube player loaded id: ', this.props.videoId);
+        const html = `
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <div id="player"></div>
+                <script>
+                var tag = document.createElement('script');
+                tag.src = "https://www.youtube.com/iframe_api";
+                var firstScriptTag = document.getElementsByTagName('script')[0];
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                var player;
+                function onYouTubeIframeAPIReady() {
+                    player = new YT.Player('player', {
+                        height: 550,
+                        width: 960,
+                        playerVars: {
+                            enablejsapi: 1,
+                            rel: 0,
+                            controls: 0,
+                            fs: 0,
+                            playsinline: 1,
+                            modestbranding: 1
+                        },
+                        videoId: '${this.props.videoId}',
+                        events: {
+                            'onReady': ${this.onPlayerReady}
+                        }
+                    });
+                }
+                </script>
+            </body>
+            </html>`;
+
+        let playerState = this.props.condition === 'play' ? `player.playVideo();` : `player.pauseVideo();` 
+
+        setTimeout(() => {
+            this.webref.injectJavaScript(playerState);
+        }, 0);
 
         return (
             <WebView
                 javascriptEnabled={true}
-                source={{ uri: youtubeUrl }}
+                useWebKit={true}
+                source={{ html }}
+                ref={r => (this.webref = r)}
+                originWhitelist={['*']}
+                allowsInlineMediaPlayback={true}
             />
         )
     }
