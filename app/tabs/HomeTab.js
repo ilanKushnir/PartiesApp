612bb2d4228b6d48b390f1320766068e11e731cb
amitb <amitbu@mta.ac.il>
2020-06-01 23:16:09 +0300

    deleted mofo

--- app/tabs/HomeTab.js
+++ app/tabs/HomeTab.js
@@ -14,8 +14,7 @@ export class HomeTab extends React.Component {
   render() {
     return (
       <View style={styles.center}>
-        <Text style={styles.title}>Hi {this.state.username}!</Text>
-        <Text style={styles.title}>Welcome back you mother fucker</Text>
+        <Text style={styles.title}>Welcome {this.state.username}!</Text>
         <Text>user stats</Text>
         <Text>user's top playlists</Text>
         <Button title="Back To Login" onPress={() => this.props.navigation.dispatch(StackActions.popToTop())}></Button>
