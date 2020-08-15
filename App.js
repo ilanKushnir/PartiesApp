76519ea72048829352557d5dc0078db2c516fe51
Ofir Cohen <ofir5300@gmail.com>
2020-08-15 18:49:12 +0300

    After merge

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Navigator from './app/views/subComponents/Navigator.js'
 window.addEventListener = x => x;   //  mock eventListener to satisfy Firestore
 console.ignoredYellowBox = ['Warning:'];
-import * as Linking from 'expo-linking';
 
 export default class App extends React.Component {
   render() {
