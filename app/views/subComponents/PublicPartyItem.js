fb9b3136a628ee7c988e980625db2932fae2cb3e
Dekel <dekelglass@gmail.com>
2020-09-05 02:23:01 +0300

    ActiveplaylistStyle

--- app/views/subComponents/PublicPartyItem.js
+++ app/views/subComponents/PublicPartyItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, TouchableOpacity, Image, Text, Alert } from 'react-native';
 import { styles } from '../../styles/styles';
+import { color } from 'react-native-reanimated';
 
 export default class PublicPartyItem extends React.Component {
     constructor(props) {
@@ -24,14 +25,26 @@ export default class PublicPartyItem extends React.Component {
                 onPress={() => {
                     this.joinParty();
                 }}>
-                <View style={this.props.condition === 'play' ? styles.partyItemPlayed : styles.partyItemPaused}>
+                <View style={{ ...styles.ActivePlaylistItem, flexDirection: "row", justifyContent: 'space-evenly', height: 60 }}>
+                    <Text style={{ flex: 6,position:'relative',left:9, fontSize:16 }}>
+                        {this.props.name}
+                    </Text>
+                    <Text style={{ flex: 6, color: this.props.condition === 'play' ? 'green' : 'red' }}>
+                        {this.props.condition}
+                    </Text>
+                    <Text style={{ flex: 2 }}>
+                        {this.props.joinId}
+                    </Text>
+                </View>
+
+                {/* <View style={this.props.condition === 'play' ? styles.partyItemPlayed : styles.partyItemPaused}>
                     <Text style={{ flex: 6 }}>
                         {this.props.joinId} - {this.props.name}
                     </Text>
                     <Text style={{ flex: 6 }}>
                         status: {this.props.condition}
                     </Text>
-                </View>
+                </View> */}
             </TouchableOpacity>
         )
     }
